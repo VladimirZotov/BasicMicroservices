da3f0ba73ee105d88e89579242c19fe23d3bc2d0
Vladimir Zotov <vladimir.a.zotov@gmail.com>
2022-06-07 08:47:59 +0300

    Added IpLatformRepo DI

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
