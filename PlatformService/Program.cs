a14de1be06253495024fbf6c5e146317d6c3b1c3
Vladimir Zotov <vladimir.a.zotov@gmail.com>
2022-06-07 08:35:20 +0300

    Added Db context

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
