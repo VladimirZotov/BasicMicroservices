a7738cedefb7820023372a7cf61de4438cbf7b2b
Vladimir Zotov <vladimir.a.zotov@gmail.com>
2022-06-16 08:26:22 +0300

    Added http client into platform service

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dto;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService.Controllers
 {
@@ -12,19 +13,32 @@ namespace PlatformService.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IPlatformRepo _platformRepo;
-        
-        public PlatformsController(IMapper mapper, IPlatformRepo platformRepo)
+        private readonly ICommandDataClient _commandDataClient;
+
+        public PlatformsController(IMapper mapper, IPlatformRepo platformRepo, ICommandDataClient commandDataClient )
         {
             _mapper = mapper;
             _platformRepo = platformRepo;
+            _commandDataClient = commandDataClient;
         }
         [HttpPost]
-        public ActionResult<PlatformReadDto> Create(PlatformCreateDto platformCreateDto)
+        public async Task<ActionResult<PlatformReadDto>> CreateAsync(PlatformCreateDto platformCreateDto)
         {
             var platform = _mapper.Map<Platform>(platformCreateDto);
             _platformRepo.CreatePlatform(platform);
             _platformRepo.SaveChanges();
+
             var platformReadDto = _mapper.Map<PlatformReadDto>(platform);
+
+            try
+            {
+                await _commandDataClient.SendPlatformToCommand(platformReadDto);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not send sync: {ex.Message}");
+            }
+
             //CreatedAtRoute returns 201 Created response and provide "location" header parameter added automatically
             return CreatedAtRoute(nameof(GetPlatformById), new { platformId = platformReadDto.Id}, platformReadDto);
         }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
